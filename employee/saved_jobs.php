675f140e4f2ec17fb701659636663c2dcf498b17
v <153298243+mizureoo@users.noreply.github.com>
2025-11-06 17:28:44 +0800

    Update saved_jobs.php

--- employee/saved_jobs.php
+++ employee/saved_jobs.php
@@ -15,7 +15,7 @@ $accountType = ucfirst($_SESSION['role']);
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Find Jobs | PRMSUmikap</title>
+<title>Saved Jobs | PRMSUmikap</title>
 
 <!-- Bootstrap & Icons -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
